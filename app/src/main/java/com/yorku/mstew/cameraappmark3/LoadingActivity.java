ae27fa935f850094ef597daa5fe394c038835799
Michael Stewart <mstewart@yorku.ca>
2017-12-14 19:17:52 -0500

    first commit

---
+++ app/src/main/java/com/yorku/mstew/cameraappmark3/LoadingActivity.java
@@ -0,0 +1,41 @@
+package com.yorku.mstew.cameraappmark3;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class LoadingActivity extends AppCompatActivity {
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        View decorView = getWindow().getDecorView();
+        if (hasFocus) {
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+            //this makes our window sticky
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loading_activity);
+
+
+    }
+
+
+
+    //Creative RGB 153,51,102
+    //Passiontate RGB 204,51,51
+    //Rational RGB RGB 0,51,102
+    //Confident
+}
